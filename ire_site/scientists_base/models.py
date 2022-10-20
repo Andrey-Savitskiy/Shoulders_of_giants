92c6f08f8939c5643df4ccd88f6afbd655c6db49
MrBlackDog <sasha29091999@yandex.ru>
2022-10-20 19:56:22 +0300

    Add ManyToManyField

--- ire_site/scientists_base/models.py
+++ ire_site/scientists_base/models.py
@@ -4,15 +4,21 @@ import datetime
 
 
 class Scientist(models.Model):
-    name = models.CharField(max_length=500, db_index=True, verbose_name='ФИО')
+    name = models.CharField(max_length=500, db_index=True, unique=True, verbose_name='ФИО')
     short_description = models.CharField(max_length=500, verbose_name='Краткое описание')
     full_description = models.CharField(max_length=500, verbose_name='Полное описание')
-    photo = models.ImageField(upload_to='photos/scientists', verbose_name='Фото')
+    photo = models.ImageField(upload_to='photos/scientists', verbose_name='Фото', blank=True)
     life_years = models.CharField(max_length=50, verbose_name='Годы жизни')
+
     start_year = models.ForeignKey('Year', null=True, on_delete=models.PROTECT,
                                    related_name='scientist_start_year', verbose_name='Год рождения')
     end_year = models.ForeignKey('Year', null=True, on_delete=models.PROTECT,
                                  related_name='scientist_end_year', verbose_name='Год смерти')
+
+    invention = models.ManyToManyField('Invention', null=True, blank=True,
+                                       related_name='scientist_invention', verbose_name='Изобретение')
+    organization = models.ManyToManyField('Organization', null=True, blank=True,
+                                       related_name='scientist_organization', verbose_name='Организация')
 
     def __str__(self):
         return self.name
@@ -24,15 +30,22 @@ class Scientist(models.Model):
 
 
 class Organization(models.Model):
-    name = models.CharField(max_length=500, db_index=True, verbose_name='Название')
+    name = models.CharField(max_length=500, db_index=True, unique=True, verbose_name='Название')
     short_description = models.CharField(max_length=500, verbose_name='Краткое описание')
     full_description = models.CharField(max_length=500, verbose_name='Полное описание')
-    logo = models.ImageField(upload_to='photos/scientists', verbose_name='Логотип', blank=True)
+    logo = models.ImageField(upload_to='photos/organizations', verbose_name='Логотип', blank=True)
     history = models.CharField(max_length=500, verbose_name='История')
+
     start_year = models.ForeignKey('Year', null=True, on_delete=models.PROTECT,
                                    related_name='organization_start_year', verbose_name='Год создания')
     end_year = models.ForeignKey('Year', null=True, on_delete=models.PROTECT,
                                  related_name='organization_end_year', verbose_name='Год расформирования')
+
+    invention = models.ManyToManyField('Invention', null=True, blank=True,
+                                       related_name='organization_invention', verbose_name='Изобретение')
+    scientist = models.ManyToManyField('Scientist', null=True, blank=True, through=Scientist.organization.through,
+                                       related_name='organization_scientist', verbose_name='Ученый')
+
 
     def __str__(self):
         return self.name
@@ -44,11 +57,17 @@ class Organization(models.Model):
 
 
 class Invention(models.Model):
-    title = models.CharField(max_length=500, db_index=True, verbose_name='Название')
+    title = models.CharField(max_length=500, db_index=True, unique=True, verbose_name='Название')
     short_description = models.CharField(max_length=500, verbose_name='Краткое описание')
     full_description = models.CharField(max_length=500, verbose_name='Полное описание')
-    photo = models.ImageField(upload_to='photos/scientists', verbose_name='Фото', blank=True)
+    photo = models.ImageField(upload_to='photos/inventions', verbose_name='Фото', blank=True)
+
     year = models.ForeignKey('Year', null=True, on_delete=models.PROTECT, verbose_name='Год создания')
+
+    organization = models.ManyToManyField('Organization', null=True, blank=True, through=Organization.invention.through,
+                                       related_name='invention_organization', verbose_name='Организация')
+    scientist = models.ManyToManyField('Scientist', null=True, blank=True, through=Scientist.invention.through,
+                                       related_name='invention_scientist', verbose_name='Ученый')
 
     def __str__(self):
         return self.title
@@ -60,7 +79,7 @@ class Invention(models.Model):
 
 
 class Year(models.Model):
-    year = models.IntegerField(verbose_name='Год', db_index=True,
+    year = models.IntegerField(verbose_name='Год', db_index=True,  unique=True,
                                validators=[MinValueValidator(1830), MaxValueValidator(datetime.date.today().year)])
 
     def __str__(self):
