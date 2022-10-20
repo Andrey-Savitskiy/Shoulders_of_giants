b7cedf9c9921f2ab01e9854e543f14a6b591e525
MrBlackDog <sasha29091999@yandex.ru>
2022-10-20 18:41:48 +0300

    Add ForeignKey

--- ire_site/scientists_base/admin.py
+++ ire_site/scientists_base/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ScientistAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'life_years']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+    list_filter = ['name']
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+    list_filter = ['name']
+
+
+class InventionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'year']
+    list_display_links = ['id', 'title']
+    search_fields = ['title']
+    list_filter = ['title', 'year']
+
+
+class YearAdmin(admin.ModelAdmin):
+    list_display = ['id', 'year']
+    list_display_links = ['id', 'year']
+    search_fields = ['year']
+    list_filter = ['year']
+
+
+admin.site.register(Scientist, ScientistAdmin)
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Invention, InventionAdmin)
+admin.site.register(Year, YearAdmin)
--- ire_site/scientists_base/models.py
+++ ire_site/scientists_base/models.py
@@ -4,11 +4,15 @@ import datetime
 
 
 class Scientist(models.Model):
-    name = models.CharField(max_length=500, verbose_name='ФИО')
+    name = models.CharField(max_length=500, db_index=True, verbose_name='ФИО')
     short_description = models.CharField(max_length=500, verbose_name='Краткое описание')
     full_description = models.CharField(max_length=500, verbose_name='Полное описание')
     photo = models.ImageField(upload_to='photos/scientists', verbose_name='Фото')
     life_years = models.CharField(max_length=50, verbose_name='Годы жизни')
+    start_year = models.ForeignKey('Year', null=True, on_delete=models.PROTECT,
+                                   related_name='scientist_start_year', verbose_name='Год рождения')
+    end_year = models.ForeignKey('Year', null=True, on_delete=models.PROTECT,
+                                 related_name='scientist_end_year', verbose_name='Год смерти')
 
     def __str__(self):
         return self.name
@@ -19,11 +23,32 @@ class Scientist(models.Model):
         ordering = ['name']
 
 
-class Invention(models.Model):
-    title = models.CharField(max_length=500, verbose_name='Название')
+class Organization(models.Model):
+    name = models.CharField(max_length=500, db_index=True, verbose_name='Название')
     short_description = models.CharField(max_length=500, verbose_name='Краткое описание')
     full_description = models.CharField(max_length=500, verbose_name='Полное описание')
-    photo = models.ImageField(upload_to='photos/scientists', verbose_name='Фото')
+    logo = models.ImageField(upload_to='photos/scientists', verbose_name='Логотип', blank=True)
+    history = models.CharField(max_length=500, verbose_name='История')
+    start_year = models.ForeignKey('Year', null=True, on_delete=models.PROTECT,
+                                   related_name='organization_start_year', verbose_name='Год создания')
+    end_year = models.ForeignKey('Year', null=True, on_delete=models.PROTECT,
+                                 related_name='organization_end_year', verbose_name='Год расформирования')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Организация'
+        verbose_name_plural = 'Организации'
+        ordering = ['name']
+
+
+class Invention(models.Model):
+    title = models.CharField(max_length=500, db_index=True, verbose_name='Название')
+    short_description = models.CharField(max_length=500, verbose_name='Краткое описание')
+    full_description = models.CharField(max_length=500, verbose_name='Полное описание')
+    photo = models.ImageField(upload_to='photos/scientists', verbose_name='Фото', blank=True)
+    year = models.ForeignKey('Year', null=True, on_delete=models.PROTECT, verbose_name='Год создания')
 
     def __str__(self):
         return self.title
@@ -35,13 +60,13 @@ class Invention(models.Model):
 
 
 class Year(models.Model):
-    date = models.IntegerField(verbose_name='Год',
-                               validators=[MinValueValidator(1930), MaxValueValidator(datetime.date.today().year)])
+    year = models.IntegerField(verbose_name='Год', db_index=True,
+                               validators=[MinValueValidator(1830), MaxValueValidator(datetime.date.today().year)])
 
     def __str__(self):
-        return self.date
+        return str(self.year)
 
     class Meta:
         verbose_name = 'Год'
         verbose_name_plural = 'Годы'
-        ordering = ['-date']
+        ordering = ['-year']
