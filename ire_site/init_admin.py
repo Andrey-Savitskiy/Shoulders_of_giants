9f8fa633118f4a3d000f6d997e9e761f8af1012c
MrBlackDog <sasha29091999@yandex.ru>
2022-11-10 16:38:57 +0300

    Add docker.service 1.0

--- ire_site/init_admin.py
+++ ire_site/init_admin.py
@@ -2,5 +2,14 @@ import os
 from django.contrib.auth import get_user_model
 
 
+username = os.getenv('ADMIN_LOGIN')
+email = os.getenv('ADMIN_EMAIL')
+password = os.getenv('ADMIN_PASS')
+
 User = get_user_model()
-User.objects.create_superuser(os.getenv('ADMIN_LOGIN'), os.getenv('ADMIN_EMAIL'), os.getenv('ADMIN_PASS'))
+if not User.objects.filter(username=username).exists():
+    print('Creating account for %s (%s)' % (username, email))
+    User.objects.create_superuser(
+        email=email, username=username, password=password)
+else:
+    print('Admin account has already been initialized.')
